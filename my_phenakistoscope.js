1cd48993a5c69c856c74e87262c28e8154b0dd8f
LouisEckardt <eckardloui@myvuw.ac.nz>
2023-09-14 16:33:21 +1200

    changing location and background colour

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -12,7 +12,7 @@ function setup_pScope(pScope){
 
 function setup_layers(pScope){
 
-  new PLayer(null, 140);  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null, 245, 245, 220 );  //lets us draw the whole circle background, ignoring the boundaries
 
   var draw_peaSequence = new PLayer(draw_pea);
   draw_peaSequence.mode( RING );
@@ -26,7 +26,7 @@ function setup_layers(pScope){
 
 function draw_pea(x, y, animation, pScope){
   scale(1.5);
- pScope.draw_image_from_sequence("draw_pea", x, -280, animation.frame); 
+ pScope.draw_image_from_sequence("draw_pea", x, -400, animation.frame); 
 
   //sscale(animation.wave(2));
 
@@ -42,5 +42,6 @@ function squares(x, y, animation, pScope){
   let backgroundArcStart = 270 - angleOffset;
   let backgroundArcEnd = 270 + angleOffset;
 
-  
+  line (100, 100, 100, 100);
+ 
 }
